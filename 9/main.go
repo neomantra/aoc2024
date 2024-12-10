986e625accb9882ab1c3828fdbf7b893c551ccc3
Evan Wies <evan@neomantra.net>
2024-12-09 20:31:58 -0500

    Day 9, part 2
    Signed-off-by: Evan Wies <evan@neomantra.net>

--- 9/main.go
+++ 9/main.go
@@ -11,6 +11,13 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
+func minOf(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
 func fillSlice[T any](t T, size int) []T {
 	s := make([]T, size)
 	for i := range s {
@@ -33,10 +40,12 @@ func NewFilesystem(puzzle string) *Filesystem {
 	for _, c := range puzzle {
 		filesystem.diskMap = append(filesystem.diskMap, byte(c)-byte('0'))
 	}
+	filesystem.makeFileMap()
+
 	return &filesystem
 }
 
-func (fs *Filesystem) MakeFileMap() {
+func (fs *Filesystem) makeFileMap() {
 	var fileMap []int
 	curID := 0
 	for i, b := range fs.diskMap {
@@ -50,7 +59,19 @@ func (fs *Filesystem) MakeFileMap() {
 	fs.fileMap = fileMap
 }
 
-func (fs *Filesystem) Defrag() {
+func (fs *Filesystem) CalcChecksum() int {
+	sum := 0
+	for i, val := range fs.fileMap {
+		if val != 0 {
+			sum += i * (val - 1) // ID is val-1
+		}
+	}
+	return sum
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+func (fs *Filesystem) DefragBlock() {
 	// forward scan filemap
 	lastJ := len(fs.fileMap) - 1 // last searched end
 	for i := 0; i < lastJ; i++ {
@@ -71,14 +92,60 @@ func (fs *Filesystem) Defrag() {
 	}
 }
 
-func (fs *Filesystem) CalcChecksum() int {
-	sum := 0
-	for i, val := range fs.fileMap {
-		if val != 0 {
-			sum += i * (val - 1) // ID is val-1
+///////////////////////////////////////////////////////////////////////////////
+
+// returns -1 if not found
+func (fs *Filesystem) searchForwardForFreespace(lastIndex int, spaceSize int) int {
+	// search forward for free space
+	for i := 0; i < lastIndex; i++ {
+		if fs.fileMap[i] == 0 {
+			// we found some free space, now search forward for the end of it
+			count := 1
+			freeIndex := i
+			for i = i + 1; i < minOf(freeIndex+spaceSize, lastIndex); i++ {
+				if fs.fileMap[i] != 0 {
+					i--
+					break
+				}
+				count++
+			}
+			if count >= spaceSize {
+				return freeIndex
+			}
 		}
 	}
-	return sum
+	return -1
+}
+
+// returns true if we made a change
+func (fs *Filesystem) DefragWholeFile() {
+	// reverse scan for a file
+	for i := len(fs.fileMap) - 1; i > 0; i-- {
+		ival := fs.fileMap[i]
+		if ival == 0 {
+			continue
+		}
+		// we found a file... now search backward for its start
+		fileSize, fileIndex := 1, i
+		for i = i - 1; i > 0; i-- {
+			if fs.fileMap[i] != ival {
+				// we are at next file
+				i++
+				break
+			}
+			fileSize++
+			fileIndex = i
+		}
+		// we are at a file, now search forward for free space for it
+		freeIndex := fs.searchForwardForFreespace(fileIndex, fileSize)
+		if freeIndex != -1 {
+			// move file to free space
+			for j := 0; j < fileSize; j++ {
+				fs.fileMap[freeIndex+j] = fs.fileMap[fileIndex+j]
+				fs.fileMap[fileIndex+j] = 0
+			}
+		}
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -131,9 +198,13 @@ func main() {
 	}
 
 	// part 1
-	fs.MakeFileMap()
-	//fmt.Println(fs.View())
-	fs.Defrag()
-	//fmt.Println(fs.View())
+	fs.DefragBlock()
 	fmt.Println("9.1:", fs.CalcChecksum())
+
+	// part 1
+	fs = NewFilesystem(string(diskMapData))
+	//fmt.Println(fs.View())
+	fs.DefragWholeFile()
+	//fmt.Println(fs.View())
+	fmt.Println("9.2:", fs.CalcChecksum())
 }
