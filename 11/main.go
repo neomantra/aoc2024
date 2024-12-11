e07cf828f2a4e320100cf1071e76e71c516625ed
Evan Wies <evan@neomantra.net>
2024-12-11 12:03:49 -0500

    Day 11, part 2
    Signed-off-by: Evan Wies <evan@neomantra.net>

--- 11/main.go
+++ 11/main.go
@@ -6,17 +6,41 @@ package main
 import (
 	"fmt"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
 )
 
 func tenToPower(n int) int {
+	switch n {
+	case 0:
+		return 1
+	case 1:
+		return 10
+	case 2:
+		return 100
+	case 3:
+		return 1000
+	case 4:
+		return 10000
+	case 5:
+		return 100000
+	case 6:
+		return 1000000
+	}
 	result := 1
 	for i := 0; i < n; i++ {
 		result *= 10
 	}
 	return result
+}
+
+func countDigits(num int) int {
+	numDigits := 0
+	for num > 0 {
+		num /= 10
+		numDigits++
+	}
+	return numDigits
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -38,33 +62,80 @@ func NewStoneRow(puzzle string) *StoneRow {
 }
 
 func (sr *StoneRow) Blink() {
-	lastIndex := len(sr.stones)
-	for i := 0; i < lastIndex; i++ {
+	newStones := make([]int, 0, len(sr.stones)*2)
+	for i := 0; i < len(sr.stones); i++ {
 		stone := sr.stones[i]
 		if stone == 0 {
-			sr.stones[i] = 1
+			newStones = append(newStones, 1)
 			continue
 		}
 
-		num, numDigits := stone, 0
-		for num > 0 {
-			num /= 10
-			numDigits++
-		}
+		numDigits := countDigits(stone)
 
-		if numDigits%2 != 0 {
-			sr.stones[i] *= 2024
+		// if numDigits%2 != 0 {
+		if (numDigits & 1) != 0 {
+			newStones = append(newStones, stone*2024)
 		} else {
 			// split odd stone in half
 			factor := tenToPower(numDigits / 2)
 			a := stone / factor
 			b := stone - (a * factor)
-			sr.stones[i] = a
-			sr.stones = slices.Insert(sr.stones, i+1, b)
-			lastIndex++
-			i++
+			newStones = append(newStones, a, b)
 		}
 	}
+	sr.stones = newStones
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+type Pair struct {
+	Stone     int
+	NumBlinks int
+}
+
+func NewPair(stone, numBlinks int) Pair { return Pair{Stone: stone, NumBlinks: numBlinks} }
+
+var breakTimesMemo = make(map[Pair]int)
+
+func breakTimes(stone int, numBlinks int) int {
+	if numBlinks <= 0 {
+		return 1
+	}
+	cachedCount, ok := breakTimesMemo[NewPair(stone, numBlinks)]
+	if ok {
+		return cachedCount
+	}
+
+	if stone == 0 {
+		count := breakTimes(1, numBlinks-1)
+		breakTimesMemo[NewPair(stone, numBlinks)] = count
+		return count
+	}
+
+	numDigits := countDigits(stone)
+	if (numDigits & 1) != 0 {
+		count := breakTimes(stone*2024, numBlinks-1)
+		breakTimesMemo[NewPair(stone, numBlinks)] = count
+		return count
+	} else {
+		// split odd stone in half
+		factor := tenToPower(numDigits / 2)
+		a := stone / factor
+		b := stone - (a * factor)
+		count := breakTimes(a, numBlinks-1) + breakTimes(b, numBlinks-1)
+		breakTimesMemo[NewPair(stone, numBlinks)] = count
+		return count
+	}
+}
+
+func (sr *StoneRow) CountAfterBlinking(numBlinks int) int {
+	// go over each stone, processing it numBlinks times
+	numStones := 0
+	for i := 0; i < len(sr.stones); i++ {
+		stone := sr.stones[i]
+		numStones += breakTimes(stone, numBlinks)
+	}
+	return numStones
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -95,9 +166,11 @@ func main() {
 
 	// part 1
 	fmt.Println(stoneRow.View())
-	for i := 0; i < 25; i++ {
-		stoneRow.Blink()
-		fmt.Println(stoneRow.View())
-	}
-	fmt.Println("11.1:", len(stoneRow.stones))
+	count := stoneRow.CountAfterBlinking(25)
+	fmt.Println("11.1:", count)
+
+	// part 2
+	stoneRow = NewStoneRow(string(stoneData))
+	count = stoneRow.CountAfterBlinking(75)
+	fmt.Println("11.2:", count)
 }
