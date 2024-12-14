15c0171abba1580371df3cfa3beff874021fe4eb
Evan Wies <evan@neomantra.net>
2024-12-14 08:25:47 -0500

    Day 13, part 2
    Signed-off-by: Evan Wies <evan@neomantra.net>

--- 13/main.go
+++ 13/main.go
@@ -51,10 +51,15 @@ func (g ClawGame) String() string {
 	return fmt.Sprintf("{ A: %v, B: %v, P: %v", g.ButtonA, g.ButtonB, g.Prize)
 }
 
+func (g *ClawGame) ApplyConversion() {
+	g.Prize.X += 10000000000000
+	g.Prize.Y += 10000000000000
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 
 // Finds the cheapest play... returns 0 if cannot win
-func (g ClawGame) CheapestPlay() int {
+func (g ClawGame) CheapestPlayBrute() int {
 	// brute force since small range
 	const MaxIters = 101
 	minScore := math.MaxInt
@@ -79,6 +84,30 @@ func (g ClawGame) CheapestPlay() int {
 	return minScore
 }
 
+func (g ClawGame) CheapestPlayLinear() int {
+	// matrix:
+	//  |Ax Bx| |Ta| = |Px|
+	//  |Ay By| |Tb| = |Py|
+	Ax, Ay, Bx, By := g.ButtonA.X, g.ButtonA.Y, g.ButtonB.X, g.ButtonB.Y
+	det := Ax*By - Ay*Bx
+	if det == 0 {
+		return 0 // no solution or we can't go backwards
+	}
+	// Cramer's rule
+	Px, Py := g.Prize.X, g.Prize.Y
+	Ta := (Px*By - Py*Bx) / det
+	Tb := (Py*Ax - Px*Ay) / det
+
+	// check if solution is valid
+	x := Ta*Ax + Tb*Bx
+	y := Ta*Ay + Tb*By
+	if x == Px && y == Py {
+		return ButtonACost*Ta + ButtonBCost*Tb
+	} else {
+		return 0
+	}
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 
 func main() {
@@ -88,18 +117,28 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err.Error())
 		os.Exit(1)
 	}
+
+	// part 1
 	games := NewClawGames(string(clawGameData))
 	if games == nil {
 		fmt.Fprintf(os.Stderr, "Bad claw game data\n")
 		os.Exit(1)
 	}
-
-	// part 1
 	cost := 0
 	for _, game := range games {
-		thisCost := game.CheapestPlay()
+		thisCost := game.CheapestPlayBrute()
 		cost += thisCost
-		fmt.Println(game.String(), thisCost)
 	}
 	fmt.Println("13.1:", cost)
+
+	// part 2
+	for i := 0; i < len(games); i++ {
+		games[i].ApplyConversion()
+	}
+	cost = 0
+	for _, game := range games {
+		thisCost := game.CheapestPlayLinear()
+		cost += thisCost
+	}
+	fmt.Println("13.2:", cost)
 }
