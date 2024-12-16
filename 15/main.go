7de041287addb7eef0bc116dadc8911170223b3d
Evan Wies <evan@neomantra.net>
2024-12-15 22:44:14 -0500

    Day 15, part 2
    Signed-off-by: Evan Wies <evan@neomantra.net>

--- 15/main.go
+++ 15/main.go
@@ -14,6 +14,8 @@ const (
 	Box   = 'O'
 	Bot   = '@'
 	Wall  = '#'
+	LBox  = '['
+	RBox  = ']'
 
 	Up      = '^'
 	Down    = 'v'
@@ -101,12 +103,16 @@ func (w *Warehouse) MoveRobot(dir Point) bool {
 	// check next position
 	nextPos := w.RobotPos.Add(dir)
 	sideCell := w.GetCell(nextPos)
-
 	if sideCell == Wall {
 		return false
 	} else if sideCell == Box {
 		// check if the box can move
 		if !w.SlideBox(nextPos, dir) {
+			// could not move box, so don't move bot
+			return false
+		}
+	} else if sideCell == LBox || sideCell == RBox {
+		if !w.SlideExpandedBox(nextPos, dir) {
 			// could not move box, so don't move bot
 			return false
 		}
@@ -142,12 +148,132 @@ func (w *Warehouse) GPSScore() int {
 	score := 0
 	for y := 0; y < w.Extent.Y; y++ {
 		for x := 0; x < w.Extent.X; x++ {
-			if w.Map[y][x] == Box {
+			cell := w.Map[y][x]
+			if cell == Box || cell == LBox {
 				score += 100*y + x
 			}
 		}
 	}
 	return score
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+func (w *Warehouse) Expand() {
+	var newMap [][]byte // Y, X
+	for y := 0; y < w.Extent.Y; y++ {
+		var b []byte
+		for x := 0; x < w.Extent.X; x++ {
+			c := w.Map[y][x]
+			c1, c2 := c, c
+			if c == Box {
+				c1, c2 = LBox, RBox
+			} else if c == Bot {
+				c1, c2 = Bot, Empty
+				w.RobotPos = Point{x, y}
+			}
+			b = append(b, c1, c2)
+		}
+		newMap = append(newMap, b)
+	}
+	w.Map = newMap
+	w.Extent.X *= 2
+	w.RobotPos.X *= 2
+}
+
+func (w *Warehouse) TryMoveBoxVertically(boxPos Point, dir Point) bool {
+	boxCell := w.GetCell(boxPos)
+	if boxCell == Empty {
+		return true
+	} else if boxCell == Wall {
+		return false
+	}
+
+	var otherBoxPos Point
+	if boxCell == LBox {
+		otherBoxPos = boxPos.Add(Point{+1, 0})
+	} else { // boxCell == RBox
+		otherBoxPos = boxPos.Add(Point{-1, 0})
+	}
+
+	nextPos, otherNextPos := boxPos.Add(dir), otherBoxPos.Add(dir)
+	nextCell, otherNextCell := w.GetCell(nextPos), w.GetCell(otherNextPos)
+	if nextCell == Wall || otherNextCell == Wall {
+		return false
+	}
+	if (nextCell == Empty || w.TryMoveBoxVertically(nextPos, dir)) &&
+		(otherNextCell == Empty || w.TryMoveBoxVertically(otherNextPos, dir)) {
+		return true
+	}
+
+	return false
+}
+
+// we call this when we know the box can move
+func (w *Warehouse) MoveBoxVertically(boxPos Point, dir Point) bool {
+	boxCell := w.GetCell(boxPos)
+	if boxCell == Empty {
+		return true
+	} else if boxCell == Wall {
+		return false
+	}
+
+	var otherBoxPos Point
+	var otherBoxCell byte
+	if boxCell == LBox {
+		otherBoxCell = RBox
+		otherBoxPos = boxPos.Add(Point{+1, 0})
+	} else { // boxCell == RBox
+		otherBoxCell = LBox
+		otherBoxPos = boxPos.Add(Point{-1, 0})
+	}
+
+	nextPos, otherNextPos := boxPos.Add(dir), otherBoxPos.Add(dir)
+	nextCell, otherNextCell := w.GetCell(nextPos), w.GetCell(otherNextPos)
+	if nextCell == Wall || otherNextCell == Wall {
+		return false
+	}
+	if nextCell == LBox || nextCell == RBox {
+		w.MoveBoxVertically(nextPos, dir)
+	}
+	if otherNextCell == LBox || otherNextCell == RBox {
+		w.MoveBoxVertically(otherNextPos, dir)
+	}
+	w.Map[nextPos.Y][nextPos.X] = boxCell
+	w.Map[otherNextPos.Y][otherNextPos.X] = otherBoxCell
+	w.Map[boxPos.Y][boxPos.X] = Empty
+	w.Map[otherBoxPos.Y][otherBoxPos.X] = Empty
+	return false
+}
+
+func (w *Warehouse) SlideExpandedBox(boxPos Point, dir Point) bool {
+	// moving left/right
+	if dir.X != 0 {
+		nextPos := boxPos.Add(dir)
+		nextNextPos := nextPos.Add(dir)
+		nextNextCell := w.GetCell(nextNextPos)
+		if nextNextCell == Wall {
+			return false
+		}
+		if (nextNextCell == LBox || nextNextCell == RBox) &&
+			!w.SlideExpandedBox(nextNextPos, dir) {
+			return false
+		}
+
+		thisCell := w.GetCell(boxPos)
+		sideCell := w.GetCell(nextPos)
+		w.Map[boxPos.Y][boxPos.X] = Empty
+		w.Map[nextPos.Y][nextPos.X] = thisCell
+		w.Map[nextNextPos.Y][nextNextPos.X] = sideCell
+		return true
+	}
+
+	// moving up/down
+	if w.TryMoveBoxVertically(boxPos, dir) {
+		w.MoveBoxVertically(boxPos, dir)
+		return true
+	}
+	return false
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -170,4 +296,17 @@ func main() {
 	warehouse.Operate()
 	fmt.Print(warehouse.View(), "\n")
 	fmt.Println("15.1:", warehouse.GPSScore())
+
+	// part 2
+	warehouse = NewWarehouse(string(warehouseData))
+	if warehouse == nil {
+		fmt.Fprintf(os.Stderr, "Bad warehouse data\n")
+		os.Exit(1)
+	}
+	fmt.Print("\n\nPart 2\n", warehouse.View(), "\n\n")
+	warehouse.Expand()
+	fmt.Print(warehouse.View(), "\n")
+	warehouse.Operate()
+	fmt.Print(warehouse.View(), "\n")
+	fmt.Println("15.2:", warehouse.GPSScore())
 }
